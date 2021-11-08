97b1089dce9c87b61707e14394e8a9f05d2d8aaa
Jawad Rafique <jawadrafique07@gmail.com>
2021-11-08 18:01:44 +0400

    order error

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useLocation } from "react-router";
 import { Link } from "react-router-dom";
-import { userRequest } from "../RequestMethods";
+import { userRequest, publicRequest } from "../RequestMethods";
 
 const Success = () => {
     const location = useLocation();
@@ -21,13 +21,15 @@ const Success = () => {
                     "Current User",
                     currentUser ? currentUser._id : "Unknown"
                 );
-                const res = await userRequest
+                const res = await publicRequest
                     .post("/orders", {
                         userId: currentUser ? currentUser._id : "Unknown",
-                        products: cart.products.map((item) => ({
-                            productId: item._id,
-                            quantity: item._quantity,
-                        })),
+                        products: [
+                            cart.products.map((item) => ({
+                                productId: item._id,
+                                quantity: item._quantity,
+                            })),
+                        ],
                         amount: cart.total,
                         address: stripeData.billing_details.address,
                     })
